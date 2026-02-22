dd1914e1c7f8f9b84049be1b1fe76925901fd937
tendryAxel <hei.tendry@gmail.com>
2026-02-22 06:23:57 -0800

    feat: player input movement

---
+++ Assets/Prefabs/player/PlayerInputSc.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputSc : MonoBehaviour
+{
+    public int FPS = 60;
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float lookSensitivity = 0.5f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    float yRotation;
+    float xRotation;
+    float currentXRotation;
+    float currentYRotation;
+    float yRotationV;
+    float xRotationV;
+    float lookSmoothnes = .1f;
+
+    public InputActionReference directionActionReference;
+    public InputActionReference jumpActionReference;
+    public InputActionReference turnCameraReference;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        Application.targetFrameRate = FPS;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        controller = GetComponent<CharacterController>();
+    }
+
+    void OnEnable()
+    {
+        directionActionReference.action.performed += DirectionAction;
+        directionActionReference.action.canceled += DirectionActionCancel;
+
+        jumpActionReference.action.performed += JumpAction;
+
+        turnCameraReference.action.performed += TurnAction;
+    }
+
+    void OnDisable()
+    {
+        directionActionReference.action.performed -= DirectionAction;
+        directionActionReference.action.canceled -= DirectionActionCancel;
+        
+        jumpActionReference.action.performed -= JumpAction;
+        
+        turnCameraReference.action.performed -= TurnAction;
+    }
+
+    void DirectionAction(InputAction.CallbackContext callback)
+    {
+        ChangeMove(callback.ReadValue<Vector2>());
+    }
+
+    void JumpAction(InputAction.CallbackContext callback)
+    {
+        ChangeJump();
+    }
+
+    void TurnAction(InputAction.CallbackContext callback)
+    {
+        Vector2 look = callback.ReadValue<Vector2>();
+        ChangeRotation(look);
+    }
+
+    void DirectionActionCancel(InputAction.CallbackContext callback)
+    {
+        ResetChangeMove();
+    }
+
+    void ChangeJump()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection.y = jumpSpeed;
+        }
+    }
+
+    void ChangeMove(Vector2 direction)
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(direction.x, 0, direction.y);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+        }
+    }
+
+    void ResetChangeMove()
+    {
+        moveDirection = Vector2.zero;
+    }
+
+    void ChangeRotation(Vector2 mouseMove)
+    {
+        yRotation += mouseMove.x * lookSensitivity;
+        xRotation -= mouseMove.y * lookSensitivity;
+        xRotation = Mathf.Clamp(xRotation, -80, 100);
+        currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothnes);
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothnes);
+    }
+
+    void Update()
+    {
+        moveDirection.y -= gravity * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
