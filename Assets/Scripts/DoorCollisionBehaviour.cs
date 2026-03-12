fbf543157980be4e595f3bb9c01b4100f157f279
tendryAxel <hei.tendry@gmail.com>
2026-03-12 07:43:54 -0700

    chore: enter houses

---
+++ Assets/Scripts/DoorCollisionBehaviour.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorCollisionBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneAfterPassingTheDoor;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collide with " + other);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Load scene " + sceneAfterPassingTheDoor);
+            SceneManager.LoadScene(sceneAfterPassingTheDoor);
+        }
+    }
+}
